b44ede632b9e1b193f66af3b20fc45d0ac5a6ae2
Claire Condon <werethylacine@gmail.com>
2017-03-30 19:43:44 -0500

    Finished studio 3, added images and bootstrap

--- js/flicklist.js
+++ js/flicklist.js
@@ -9,16 +9,18 @@ var model = {
 var api = {
 
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "a910d1e0bc83771f71769959aaaa9506", //api key
 
   /**
    * Given a movie object, returns the url to its poster image
    */
   posterUrl: function(movie) {
-    // TODO 4b
+    // TODA 4b
     // implement this function
-
-    return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg" 
+    url = "http://image.tmdb.org/t/p/w300/" + movie.poster_path;
+    console.log(url);
+    return url;
+    //return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg"
   }
 }
 
@@ -37,14 +39,14 @@ function discoverMovies(callback) {
     success: function(response) {
       model.browseItems = response.results;
       callback(response);
-      console.log(response);
+      //console.log(response);
     }
   });
 }
 
 
 /**
- * Makes an AJAX request to the /search endpoint of the API, using the 
+ * Makes an AJAX request to the /search endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, updates model.browseItems appropriately and then invokes
@@ -77,23 +79,40 @@ function render() {
   // insert watchlist items
   model.watchlistItems.forEach(function(movie) {
     var title = $("<h6></h6>").text(movie.original_title);
-
-    // TODO 1 
+    console.log(movie);
+    // TODA 1
     // add an "I watched it" button and append it below the title
     // Clicking should remove this movie from the watchlist and re-render
+    var watchedButton = $("<button>I watched it</button>")
+    .click(function() {
+      var index = model.watchlistItems.indexOf(movie);
+      model.watchlistItems.splice(index, 1);
+      render();
+    });
+    title.append(watchedButton);
 
-    // TODO 2i
+    // TODA 2i
     // apply the classes "btn btn-danger" to the "I watched it button"
+    watchedButton.addClass("btn btn-danger");
 
-    // TODO 4a
-    // add a poster image and append it inside the 
+    // TODA 4a
+    // add a poster image and append it inside the
     // panel body above the button
+    var posterIMG = $("<img></img>").attr("src", api.posterUrl(movie));
 
-    // TODO 2g
+    // TODA 2g
     // re-implement the li as a bootstrap panel with a heading and a body
     var itemView = $("<li></li>")
-      .append(title)
-      .attr("class", "item-watchlist");
+      .attr("class", "item-watchlist")
+
+
+    var panelDiv = $("<div class='panel panel-default'></div>");
+    var panelHead = $("<div class='panel-heading'></div>").text(movie.original_title);
+    var panelBody = $("<div class='panel-body'></div>").append(posterIMG).append(watchedButton);
+
+    panelDiv.append(panelHead);
+    panelDiv.append(panelBody);
+    itemView.append(panelDiv);
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -101,15 +120,15 @@ function render() {
   // insert browse items
   model.browseItems.forEach(function(movie) {
 
-    // TODO 2d continued
+    // TODA 2d continued
     // style this list item to look like the demo
     // You'll also need to make changes in index.html.
     // use the following BS classes:
-    // "list-group", "list-group-item", btn", "btn-primary", 
+    // "list-group", "list-group-item", btn", "btn-primary",
 
     var title = $("<h4></h4>").text(movie.original_title);
 
-    var button = $("<button></button>")
+    var button = $("<button class='btn btn-primary'></button>")
       .text("Add to Watchlist")
       .click(function() {
         model.watchlistItems.push(movie);
@@ -120,15 +139,15 @@ function render() {
     var overview = $("<p></p>").text(movie.overview);
 
     // append everything to itemView, along with an <hr/>
-    var itemView = $("<li></li>")
+    var itemView = $("<li class='list-group-item'></li>")
       .append(title)
       .append(overview)
       .append(button);
-      
+
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
-  
+
 }
 
 

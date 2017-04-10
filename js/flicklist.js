95a4ab7b9e8612ddc500a771dcccb403c76be8da
Claire Condon <werethylacine@gmail.com>
2017-04-10 14:04:38 -0500

    Completed studio 5

--- js/flicklist.js
+++ js/flicklist.js
@@ -2,22 +2,20 @@
 
 var model = {
   watchlistItems: [],
-  browseItems: []
-
-  // TODO 
-  // add a property for the current active movie index
+  browseItems: [],
+  activeMovieIndex: 0
 }
 
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
+  token: "a910d1e0bc83771f71769959aaaa9506", // api key
   /**
    * Given a movie object, returns the url to its poster image
    */
   posterUrl: function(movie) {
     var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
-    return baseImageUrl + movie.poster_path; 
+    return baseImageUrl + movie.poster_path;
   }
 }
 
@@ -47,7 +45,7 @@ function discoverMovies(callback, keywords) {
 
 
 /**
- * Makes an AJAX request to the /search/keywords endpoint of the API, using the 
+ * Makes an AJAX request to the /search/keywords endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, invokes the supplied callback function, passing in
@@ -62,14 +60,14 @@ function searchMovies(query, callback) {
       query: query
     },
     success: function(response) {
-      console.log(response);
-    
+      //console.log(response);
+
       var keywordIDs = response.results.map(function(keywordObj) {
         return keywordObj.id;
       });
       var keywordsString = keywordIDs.join("|");
-      console.log(keywordsString);
-      
+      //console.log(keywordsString);
+
       discoverMovies(callback, keywordsString);
     }
   });
@@ -88,7 +86,7 @@ function render() {
   // render watchlist items
   model.watchlistItems.forEach(function(movie) {
     var title = $("<h6></h6>").text(movie.original_title);
-      
+
     // movie poster
     var poster = $("<img></img>")
       .attr("src", api.posterUrl(movie))
@@ -108,7 +106,7 @@ function render() {
     var panelHeading = $("<div></div>")
       .attr("class", "panel-heading")
       .append(title);
-    
+
     // panel body contains the poster and button
     var panelBody = $("<div></div>")
       .attr("class", "panel-body")
@@ -122,28 +120,58 @@ function render() {
     $("#section-watchlist ul").append(itemView);
   });
 
-  // render browse items
-  model.browseItems.forEach(function(movie) {
-    var title = $("<h4></h4>").text(movie.original_title);
-    var overview = $("<p></p>").text(movie.overview);
+  var activeMovie = model.browseItems[model.activeMovieIndex];
+  //console.log(model.activeMovieIndex);
+  //console.log("Active movie: ", activeMovie);
 
-    // button for adding to watchlist
-    var button = $("<button></button>")
-      .text("Add to Watchlist")
-      .attr("class", "btn btn-primary")
-      .click(function() {
-        model.watchlistItems.push(movie);
-        render();
-      })
-      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
+  $("#section-browse h4").text(activeMovie.original_title);
+  $("#section-browse p").text(activeMovie.overview);
 
-    var itemView = $("<li></li>")
-      .attr("class", "list-group-item")
-      .append( [title, overview, button] );
-      
-    // append the itemView to the list
-    $("#section-browse ul").append(itemView);
-  });
+  //I think this event listener is getting recursively applied over and over
+  //on every render
+  $('#add-to-watchlist').off();
+  $("#add-to-watchlist").click(function() {
+    console.log(model.watchlistItems);
+     model.watchlistItems.push(activeMovie);
+     render();
+   });
+
+   //console.log(model.browseItems);
+   var posters = model.browseItems.map(function(movie) {
+    // returns a list item with an img inside
+    var poster_li = $("<li class='item'></li>");
+    var poster = $("<img></img>")
+      .attr("src", api.posterUrl(movie));
+    poster_li.append(poster);
+    return poster_li;
+   });
+   posters[model.activeMovieIndex].addClass("active");
+
+   $(".carousel-inner").append(posters);
+
+
+  // // render browse items
+  // model.browseItems.forEach(function(movie) {
+  //   var title = $("<h4></h4>").text(movie.original_title);
+  //   var overview = $("<p></p>").text(movie.overview);
+  //
+  //   // button for adding to watchlist
+  //   var button = $("<button></button>")
+  //     .text("Add to Watchlist")
+  //     .attr("class", "btn btn-primary")
+  //     .click(function() {
+  //       model.watchlistItems.push(movie);
+  //       render();
+  //     })
+  //     .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
+  //
+  //   var itemView = $("<li></li>")
+  //     .attr("class", "list-group-item")
+  //     .append( [title, overview, button] );
+  //
+  //   // append the itemView to the list
+  //   //$("#section-browse ul").append(itemView);
+  // });
 }
 
 
